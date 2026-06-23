27881e15e93d9f401fbb58b7afb139355c5ab56c
documentHarry <har.francis1@gmail.com>
2026-06-23 13:56:42 +0200

    fix: correction du routerLink vers l'ajout de catégorie

--- frontend/src/app/pages/categories/categorie-ajouter/categorie-ajouter.component.ts
+++ frontend/src/app/pages/categories/categorie-ajouter/categorie-ajouter.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CategorieService } from '../../../services/api/categorie.service';
 import { CategorieCreateRequest } from '../../../services/api/models/categorie-create-request.model';
 
 @Component({
   selector: 'app-categorie-ajouter',
-  imports: [ ReactiveFormsModule, ],
+  imports: [ ReactiveFormsModule, RouterLink],
   templateUrl: './categorie-ajouter.component.html',
   styleUrls: ['./categorie-ajouter.component.css']
 })
